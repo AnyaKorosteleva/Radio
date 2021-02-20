905838fa0af5226871cb6693c261ab2cb16dbd10
AnyaKorosteleva <annakorosteleva3008@gmail.com>
2021-02-21 01:38:21 +0300

    JaCoCo edited and SetChannel added

--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -8,49 +8,62 @@ class RadioTest {
     Radio radio = new Radio();
 
     @Test
-    public void shouldNextCurrentChannel () {
+    public void shouldNextCurrentChannel() {
         radio.setCurrentChannel(2);
         radio.nextChannel();
-        assertEquals(3,radio.getCurrentChannel());
+        assertEquals(3, radio.getCurrentChannel());
     }
+
     @Test
-    public void shouldNextMaxCurrentChannel () {
+    public void shouldNextMaxCurrentChannel() {
         radio.setCurrentChannel(9);
         radio.nextChannel();
         assertEquals(0, radio.getCurrentChannel());
     }
+
     @Test
-    public void shouldPrevCurrentChannel () {
+    public void shouldPrevCurrentChannel() {
         radio.setCurrentChannel(2);
         radio.prevChannel();
-        assertEquals(1,radio.getCurrentChannel());
+        assertEquals(1, radio.getCurrentChannel());
     }
+
     @Test
-    public void shouldPrevMinCurrentChannel () {
+    public void shouldPrevMinCurrentChannel() {
         radio.setCurrentChannel(0);
         radio.prevChannel();
         assertEquals(9, radio.getCurrentChannel());
     }
+
     @Test
-    public void shouldNextCurrentVolume () {
+    public void shouldSetCurrentChannel() {
+        radio.setCurrentChannel(3);
+        assertEquals(3, radio.getCurrentChannel());
+    }
+
+    @Test
+    public void shouldNextCurrentVolume() {
         radio.setCurrentVolume(5);
         radio.plusVolume();
         assertEquals(6, radio.getCurrentVolume());
     }
+
     @Test
-    public void shouldNextMaxCurrentVolume () {
+    public void shouldNextMaxCurrentVolume() {
         radio.setCurrentVolume(10);
         radio.plusVolume();
         assertEquals(10, radio.getCurrentVolume());
     }
+
     @Test
-    public void shouldPrevCurrentVolume () {
+    public void shouldPrevCurrentVolume() {
         radio.setCurrentVolume(7);
         radio.minusVolume();
         assertEquals(6, radio.getCurrentVolume());
     }
+
     @Test
-    public void shouldPrevMinCurrentVolume () {
+    public void shouldPrevMinCurrentVolume() {
         radio.setCurrentVolume(0);
         radio.minusVolume();
         assertEquals(0, radio.getCurrentVolume());
